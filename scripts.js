f9817c12a4ab6fa6bbfdebaabf24094950c3f2a7
JLZ-coder <chengliu@ucm.es>
2021-09-10 17:52:10 +0200

    finished console version of the game

--- scripts.js
+++ scripts.js
@@ -5,4 +5,59 @@ function computerPlay() {
     return VALID_CHOICES[randomChoice];
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+    
+    let roundResult = "LOSE";
+    let returnPhrase = "";
+    if (typeof playerSelection == "string") {
+        playerSelection = playerSelection.slice(0,1).toUpperCase() + playerSelection.toLowerCase().slice(1);
+    }
+    else {
+        return `Choose between ${VALID_CHOICES}`;
+    }
+
+    return evaluateChoices(playerSelection, computerSelection);
+}
+
+function evaluateChoices(playerSelection, computerSelection) {
+
+    if (playerSelection == computerSelection) {
+        roundResult = "DRAW";
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Scissors") {
+            roundResult = "WIN";
+    }
+    else if (playerSelection == "Scissors" && computerSelection == "Paper") {
+            roundResult = "WIN";
+    }
+    else if (playerSelection == "Paper" && computerSelection == "Rock") {
+            roundResult = "WIN";
+    }
+
+
+    if (roundResult == "WIN") {
+        returnPhrase = `You win! ${playerSelection} beats ${computerSelection}`;
+    }
+    else if (roundResult == "LOSE") {
+        returnPhrase = `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
+    else {
+        returnPhrase = `It's a tie ${playerSelection} equals ${computerSelection}`;
+    }
+
+    return returnPhrase;
+}
+
+function game(rounds) {
+
+    let playerSelection;
+    let computerSelection;
+
+    for (let i = 1; i <= rounds; i++) {
+        playerSelection = prompt(`Make your choice!(${VALID_CHOICES})`);
+        computerSelection = computerPlay();
+        console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game(5);
